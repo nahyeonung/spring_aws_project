f94fc6441aeb8cc116ceebf3c6b19044d49d72fb
nahyeonung <ntweety@naver.com>
2023-03-03 17:26:38 +0900

    h2 database 연결

--- study-spring/study-spring/src/main/java/com/study/studyspring/SpringConfig.java
+++ study-spring/study-spring/src/main/java/com/study/studyspring/SpringConfig.java
@@ -1,13 +1,22 @@
 package com.study.studyspring;
 
+import com.study.studyspring.repository.JdbcMemberRepository;
 import com.study.studyspring.repository.MemberRepository;
-import com.study.studyspring.repository.MemoryMemberRepository;
 import com.study.studyspring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -15,6 +24,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
