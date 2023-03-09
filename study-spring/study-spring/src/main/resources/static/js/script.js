4b7f4d806681bc04c294f3bb4e400fd14152ef21
hyeonung <ntweety@naver.com>
2023-03-09 23:44:46 +0900

    sidebar 크기에 맞춰서 반응형으로 크기 조절

--- study-spring/study-spring/src/main/resources/static/js/script.js
+++ study-spring/study-spring/src/main/resources/static/js/script.js
@@ -1,18 +1,23 @@
-function time() {
+const clock = document.querySelector('.clock');
+function getTime() {
     let today = new Date();
     let hours = today.getHours();
     let minutes = today.getMinutes();
     let seconds = today.getSeconds();
+    clock.innerHTML = `${hours<10 ? `0${hours}`:hours}:${minutes<10 ? `0${minutes}`:minutes}:${seconds<10 ? `0${seconds}`:seconds}`
 }
-
+function init(){
+    setInterval(getTime, 1000);
+}
+init();
 function cnt() {
     var s = parseInt($("#cnt").val())
     s += 1
     $("#cnt").val(s)
 }
 
-if(data != null) {
-   alert(data);
-}
+//if(data != null) {
+//   alert(data);
+//}
 
 
